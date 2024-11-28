f07f5b0ab3ba805164506d6419ec8031846bc549
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-28 10:46:52 +0700

    upload

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,24 @@
 import express from 'express'
 import { protectedRoute } from '../middleware/authMiddleware.js'
+import multer from 'multer'
+import path from 'path'
+import User from '../models/userModel.js'
+import Post from '../models/postModel.js'
 
 const router = express.Router()
+
+//set up storage engine using multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+//initialize upload variable with the storage engine
+const upload = multer({ storage: storage })
 
 //route for posts page
 router.get('/posts', protectedRoute, (req, res) => {
@@ -21,6 +38,33 @@ router.get('/edit-post/:id', protectedRoute, (req, res) => {
 //route for view posts page
 router.get('/post/:id', (req, res) => {
     return res.render('posts/view-post', { title: 'View Post', active: 'view_post' })
+})
+
+//handle create new post request
+router.post('/create-post', protectedRoute, upload.single('image'), async function (req, res) {
+    try {
+        const title = req.body.title
+        const content = req.body.content
+        const image = req.file.filename
+        const slug = title.replace(/\s+/g, '_').toLowerCase()
+
+        const user = await User.findById(req.session.user._id)
+
+        //create new post
+        const post = new Post({ title, content, slug, image, user })
+
+        //Save post in user posts object
+        await User.updateOne({ _id: req.session.user._id }, { $push: { posts: post.id } })
+
+        await post.save()
+        req.flash('success', 'Post created successfully!')
+        return res.redirect('/posts')
+
+    } catch (error) {
+        console.error(error)
+        req.flash('error', 'Something went wrong.try again!')
+        return req.redirect('/create-post')
+    }
 })
 
 export default router
